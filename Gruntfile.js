4fef22fa833fe5d193e860a147c2f77296f95a2d
jbrad <jason.bradley@me.com>
2013-10-02 21:56:32 -0500

    Added bower and package.json files and gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            child_theme: {
+                options: {
+                    paths: ["css/less"],
+                    yuicompress: true
+                },
+                files: {
+                    "style.css": "css/less/style.less"
+                }
+            }
+        },
+
+        watch: {
+            child_theme_less: {
+                files: [
+                    'css/less/*.less'
+                ],
+                tasks: ['less']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['watch']);
+
+};
